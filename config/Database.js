c728b4b5396762ac256cf89185c666668b0836cf
Mhd Mustafa Kamal <galaxyoneone64@gmail.com>
2022-05-29 05:44:02 +0700

    update commit

--- config/Database.js
+++ config/Database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  db: "mongodb+srv://kamal_kun75:karisma12@skincancluster.tucjtco.mongodb.net/?retryWrites=true&w=majority",
+  db: process.env.Urldb,  // change with your database host / url
 };
